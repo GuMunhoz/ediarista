bb835a6b8c557b7998d82c2568bacb0e86c7bb20
GuMunhoz <gustavo_munhoz@yahoo.com>
2021-06-27 12:35:09 -0300

    feature: cria input de pesquisa de ceps; ajustes de responsividade da tela

--- ediaristas/src/pages/index.tsx
+++ ediaristas/src/pages/index.tsx
@@ -1,6 +1,13 @@
 import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironment";
 import PageTitle from "ui/components/data-display/pageTitle/PageTitle";
 import UserInformation from "ui/components/data-display/UserInformation/UserInformation";
+import TextFieldMask from "ui/components/inputs/TextFieldMask/TextFieldMask";
+import { Button, Typography, Container } from "@material-ui/core";
+import {
+  FormElementsContainer,
+  ProfissionaisPaper,
+  ProfissionaisContainer,
+} from "ui/styles/pages/index.style";
 
 export default function Home() {
   return (
@@ -12,30 +19,53 @@ export default function Home() {
           "Preencha seu endereço e veja todos os profissionais da sua localicade"
         }
       />
-      <UserInformation
-        name={"Johnatan Lopes"}
-        picture={""}
-        rating={5}
-        description={"São Paulo"}
-      />
-      <UserInformation
-        name={"Gustavo Munhoz"}
-        picture={"http://github.com./GuMunhoz.png"}
-        rating={3}
-        description={"São Paulo"}
-      />
-      <UserInformation
-        name={"Gustavo Alves"}
-        picture={""}
-        rating={3}
-        description={"São Paulo"}
-      />
-      <UserInformation
-        name={"Nego do Borel"}
-        picture={""}
-        rating={1}
-        description={"São Paulo"}
-      />
+      <Container>
+        <FormElementsContainer>
+          <TextFieldMask
+            mask={"99999-999"}
+            label={"Digite seu CEP"}
+            fullWidth
+            variant={"outlined"}
+          />
+          <Typography color={"error"}> CEP inválido </Typography>
+
+          <Button
+            variant={"contained"}
+            color={"secondary"}
+            sx={{ width: "220px" }}
+          >
+            Buscar
+          </Button>
+        </FormElementsContainer>
+        <ProfissionaisPaper>
+          <ProfissionaisContainer>
+            <UserInformation
+              name={"Johnatan Lopes"}
+              picture={""}
+              rating={5}
+              description={"São Paulo"}
+            />
+            <UserInformation
+              name={"Gustavo Munhoz"}
+              picture={"http://github.com./GuMunhoz.png"}
+              rating={3}
+              description={"São Paulo"}
+            />
+            <UserInformation
+              name={"Gustavo Alves"}
+              picture={""}
+              rating={3}
+              description={"São Paulo"}
+            />
+            <UserInformation
+              name={"Nego do Borel"}
+              picture={""}
+              rating={1}
+              description={"São Paulo"}
+            />
+          </ProfissionaisContainer>
+        </ProfissionaisPaper>
+      </Container>
     </div>
   );
 }
